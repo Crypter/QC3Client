ce2f644e726b62a1b60a29a8189abcf59d71407d
Kristijan Gjoshev <crypter@mail.com>
2019-08-24 17:43:23 +0200

    Create QC2_demo.ino

---
+++ examples/QC2_demo/QC2_demo.ino
@@ -0,0 +1,61 @@
+#include <QC3Client.h>
+
+/*
+Components needed:
+2 x 10kOhm resistors
+2 x 2.2kOhm resistors
+
+Wiring Configuration:
+GPIO(Dp_2k2) - [2.2kOhm] -
+                           \
+                            > - USB Green wire
+                           /
+GPIO(Dp_10k) - [10kOhm]  -
+
+GPIO(Dn_2k2) - [2.2kOhm] -
+                           \
+                            > - USB White wire
+                           /
+GPIO(Dn_10k) - [10kOhm]  -
+*/
+
+#define Dp_10k 14 //USB green wire, data positive
+#define Dp_2k2 12 //USB green wire, data positive
+#define Dn_10k 13 //USB white wire, data negative
+#define Dn_2k2 4  //USB white wire, data negative
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("START");
+  
+  Serial.print("Configure pins: ");
+  if (QC3Client.configure(Dp_10k, Dp_2k2, Dn_10k, Dn_2k2)==0){
+    Serial.println("OK");
+  } else {
+    Serial.println("Failed");
+  }
+  
+  Serial.print("Initiate QuickCharge 2.0: ");
+  if (QC3Client.begin(true)==0){ //set true for QC2 compatibility mode
+    Serial.println("OK");
+  } else {
+    Serial.println("Failed");
+  }
+  delay(1000);
+}
+
+void loop() {
+  //QC2.0 supports only 5V, 9V and 12V operation
+  
+  Serial.print("Voltage: ");
+  Serial.println(QC3Client.setMillivolts(5000)) / 1000.0); //USB 5.0V
+  delay(2000);
+  
+  Serial.print("Voltage: ");
+  Serial.println(QC3Client.setMillivolts(9000)) / 1000.0); //9V Adapter
+  delay(2000);
+  
+  Serial.print("Voltage: ");
+  Serial.println(QC3Client.setMillivolts(12000)) / 1000.0); //12V Car battery
+  delay(2000);
+}
